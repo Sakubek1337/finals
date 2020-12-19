42597cd428dae9f9819b8296b546cbff9eadfebe
Sakubek1337 <73515656+Sakubek1337@users.noreply.github.com>
2020-12-20 04:31:21 +0600

    Adding floor and background

--- game.py
+++ game.py
@@ -1,18 +1,30 @@
 import pygame
 import sys
+import random
+
+
+def flr():
+    global floorx
+    screen.blit(floor, (floorx, 400))
+    screen.blit(floor, (floorx + 288, 400))
+    if floorx <= -288:
+        floorx = 0
 
 pygame.init()
-window = pygame.display.set_mode((500, 600))
-pygame.display.set_caption('Blappy Fird')
+screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
+floor = pygame.image.load('pict/floor.png').convert()
+floorx = 0
+
+background = pygame.image.load('pict/background.png').convert()
 
 while True:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    window.fill((176, 200, 50))
-    pygame.draw.rect(window, (0, 255, 255), (0, 0, 50, 200))
+    screen.blit(background, (0, 0))
+    flr()
+    floorx -= 1
     pygame.display.update()
-    clock.tick(120)
+    clock.tick(90)
